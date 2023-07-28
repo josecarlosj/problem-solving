e16636f22f4d5dfcd42bc30b5b68e2034e537416
josecarlosj <junior.edf@gmail.com>
2023-07-28 16:16:12 -0300

    solve me first

---
+++ hacker-rank/kotlin/1-SolveMeFirst.kt
@@ -0,0 +1,19 @@
+import java.util.*
+
+/*
+ * Challenge 001
+ *
+ * Complete the function solveMeFirst to compute the sum of two integers.
+ */
+
+fun solveMeFirst(a: Int, b: Int): Int {
+    return a.plus(b)
+}
+
+fun main(args: Array<String>) {
+    val sc = Scanner(System.`in`)
+    val num1 = sc.nextInt()
+    val num2 = sc.nextInt()
+    val sum = solveMeFirst(num1, num2)
+    println(sum)
+}
